1b064ed850065d3488239aed0386b0f281745bfc
Linna Zhang <lzhang0704@gmail.com>
2022-05-05 18:33:29 -0700

    fixed the clearPage function

--- script.js
+++ script.js
@@ -26,12 +26,12 @@ if(performance.navigation.type == performance.navigation.TYPE_RELOAD){
 	sets = JSON.parse(JSONset);
 	console.log(sets);
 	if(sets.length > 0){
-	for(var i = 0; i < sets.length; i++){
-		console.log(sets[i]);
-		createDropDown(sets[i].toString(),dropdown);
-		createDropDown(sets[i].toString(),dropdownDelete);
+		for(var i = 0; i < sets.length; i++){
+			console.log(sets[i]);
+			createDropDown(sets[i].toString(),dropdown);
+			createDropDown(sets[i].toString(),dropdownDelete);
+		}
 	}
-}
 }
 
 //adding notes using mouse
@@ -105,10 +105,10 @@ function deleteTask(){
 
 //highlight function
 function highlightSelection() {
-		var selected;
+	var selected;
 
-		if (window.getSelection)
-			selected = window.getSelection();
+	if (window.getSelection)
+		selected = window.getSelection();
         // else if (typeof document.selection != "undefined")
         //     selected = document.selection;
 
@@ -132,104 +132,104 @@ function highlightSelection() {
     }
 
 //shuffle function
-	function shuffle(){
-    	var ul = document.querySelector('ul');
-    	for (var i = ul.children.length; i >= 0; i--) {
-    		ul.appendChild(ul.children[Math.random() * i | 0]);
-    	}
-    }
-
-//save option
-    function save(){
-    	if(notes.length > 0){
-    	let id = prompt("Name Your Study Set!");
-    	for(var i = 0; i < notes.length; i++){
-    		let note = notes[i];
-    		if(note.id == ""){
-    			note.id = id;
-    		}
-    		
-    	}
-
-    	sets.push(id);
-    	localStorage.setItem("set",JSON.stringify(sets));
-
-    	var notesById = [];
-    	for(var i = 0; i < notes.length; i++){
-    		if(notes[i].id == id){
-    			notesById.push(notes[i]);
-    		}
-    	}
-
-
-    	localStorage.setItem(id,JSON.stringify(notesById));
-    	for(var i = 0; i < item.length; i++){
-    		item[i].remove();
-    	}
-    	createDropDown(id,dropdown);
-    	createDropDown(id,dropdownDelete);
-    }
+function shuffle(){
+	var ul = document.querySelector('ul');
+	for (var i = ul.children.length; i >= 0; i--) {
+		ul.appendChild(ul.children[Math.random() * i | 0]);
+	}
 }
 
-    function createDropDown(x,y){
-    	var setName = document.createElement("a");
-    	var setText = document.createTextNode(x);
-    	setName.setAttribute('href',"#");
-    	setName.setAttribute("class",x);
-    	setName.appendChild(setText);
-    	y.appendChild(setName);
-    	setName.addEventListener("click", function(){
-    		displaySet(x,y);
-    	})
+//save option
+function save(){
+	if(notes.length > 0){
+		let id = prompt("Name Your Study Set!");
+		for(var i = 0; i < notes.length; i++){
+			let note = notes[i];
+			if(note.id == ""){
+				note.id = id;
+			}
+			
+		}
 
-    }
+		sets.push(id);
+		localStorage.setItem("set",JSON.stringify(sets));
 
-    function displaySet(x,y){
-    	clearPage();
-    	var JSONset = localStorage.getItem(x);
-    	var notes = JSON.parse(JSONset);
-    	if(y == dropdown){
-    for(var i = 0; i < notes.length; i++){
-    		loadNote(notes[i].text);
-    	}
-    } else{
-    		deleteNote(x);
-    	
-    }
+		var notesById = [];
+		for(var i = 0; i < notes.length; i++){
+			if(notes[i].id == id){
+				notesById.push(notes[i]);
+			}
+		}
+		localStorage.setItem(id,JSON.stringify(notesById));
 
-    }
 
-    function loadNote(x){
-    	var li = document.createElement("li");
-    	li.setAttribute("class","note");
-    	addDelete(li);
-    	li.appendChild(document.createTextNode(x));
-    	ul.appendChild(li);
-    	addAnswerBox(li);
-    }
+		clearPage(item);
+		createDropDown(id,dropdown);
+		createDropDown(id,dropdownDelete);
+	}
+}
 
-    function deleteNote(x){
-    	const index = sets.indexOf(x);
-    	console.log(index);
-    	if(index > -1){
-    		sets.splice(index,1);
-    	}
-    	console.log(x);
-    	var setToDelete = document.getElementsByClassName(x);
-    	console.log(setToDelete[0]);
-    	setToDelete[0].parentNode.removeChild(setToDelete[0]);
-    	setToDelete[0].parentNode.removeChild(setToDelete[0]);
-    	localStorage.removeItem("set");
-    	localStorage.removeItem(x);
-    	localStorage.setItem("set",JSON.stringify(sets));
-    	console.log(sets)
-    }
+function createDropDown(x,y){
+	var setName = document.createElement("a");
+	var setText = document.createTextNode(x);
+	setName.setAttribute('href',"#");
+	setName.setAttribute("class",x);
+	setName.appendChild(setText);
+	y.appendChild(setName);
+	setName.addEventListener("click", function(){
+		clearPage(item);
+		displaySet(x,y);
+	})
 
-    function clearPage(){
-    	for(var i = 0; i < item.length; i++){
-    		item[i].remove();
-    	}
-    }
+}
+
+function displaySet(x,y){
+	var JSONset = localStorage.getItem(x);
+	var notes = JSON.parse(JSONset);
+	if(y == dropdown){
+		for(var i = 0; i < notes.length; i++){
+			loadNote(notes[i].text);
+		}
+	} else{
+		deleteNote(x);
+		
+	}
+
+}
+
+function loadNote(x){
+	var li = document.createElement("li");
+	li.setAttribute("class","note");
+	addDelete(li);
+	li.appendChild(document.createTextNode(x));
+	ul.appendChild(li);
+	addAnswerBox(li);
+}
+
+function deleteNote(x){
+	const index = sets.indexOf(x);
+	console.log(index);
+	if(index > -1){
+		sets.splice(index,1);
+	}
+	console.log(x);
+	var setToDelete = document.getElementsByClassName(x);
+	console.log(setToDelete[0]);
+	setToDelete[0].parentNode.removeChild(setToDelete[0]);
+	setToDelete[0].parentNode.removeChild(setToDelete[0]);
+	localStorage.removeItem("set");
+	localStorage.removeItem(x);
+	localStorage.setItem("set",JSON.stringify(sets));
+	console.log(sets)
+}
+
+function clearPage(x){
+	while(x.length > 0){
+		var i = 0;
+		x[i].parentNode.removeChild(x[i]);
+		i++;
+	}
+}
 
 
 
